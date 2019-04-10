8b1531872916880a2e66ac542190019f8712e10c
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-04-10 11:40:41 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -36,7 +36,6 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-
 		 programmer_hash[:alan_kay][:known_for]
 end
 
@@ -56,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].join
+		 programmer_hash[:dennis_ritchie][:langauges] = "C"
 end
 
 def adding_matz
@@ -81,8 +80,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP","C"]})
-
+programmer_hash = {:yukihiro_matsumoto => {
+	:known_for => "Ruby",
+	:languages => ["LISP", "C"]
+}
+}
 end
 
 def changing_alan
@@ -124,7 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] <<"Assembly"
-		 programmer_hash
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 
 end
